af52fb2c46a5339f7177413a270c3bd074bad370
Breno Marcomini <brenomarcomini@hotmail.com>
2022-11-08 21:58:05 -0300

    JSX - Conceito

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,11 +1,40 @@
 
+const titulo = <h1>Esse é o título</h1>
 
 function App() {
-  return (
-    <div>
-        Meu site
-    </div>
-  );
+    const nome = 'Breno';
+    const ramdom = Math.random();
+    const ativo = false;
+    function mostraNome(nome){
+         return nome;
+     }
+    const carro = {
+         marca: 'Ferrrari',
+         rodas: 4,
+    }
+    const estiloB = {
+         color: 'blue',
+        backgroundColor:'grey',
+    }
+
+    // Mostre os dados da aplicação, como aprensetado no vídeo
+    // Não utilize CSS externo, use o style para mudar as cores
+    // Se a situação estiver ativa pinte de verde, inativa vermelho
+    // Se o gasto for maior que 10000 mostre uma mensagem
+    return (
+    <>
+        {titulo}
+        {mostraNome('Julia')}
+        <p style={estiloB}>{nome}</p>
+        <p>{ramdom}</p>
+        <p>{new Date().getFullYear()}</p>
+        <p>{ramdom * 100}</p>
+        <p>{carro.marca}</p>
+        <p className={ativo ? 'ativo' : 'false'}></p>
+
+
+    </>
+    );
 }
 
 export default App;
