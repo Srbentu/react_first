ce79496c263f70c3445add570c2a15ebcd4a3c44
Breno Marcomini <brenomarcomini@hotmail.com>
2022-11-28 21:00:32 -0300

    use Custom Hook 2

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import useLocalStorage from "./useLocalStorage"
+import useFetch from "./useFetch";
 
 const App = () => {
     const [produto, setProduto] = useLocalStorage('produto', '')
-
+    const {request, data, loading, error} = useFetch()
     function handleProdutoClick({target}){
         setProduto(target.innerText)
     }
+    React.useEffect(() => {
+        async function fetchData(){
+            const {response, json} = await request('https://ranekapi.origamid.dev/json/api/produto/')
+        }
+        fetchData()
+    }, [request])
+    if (error) return <p>{error}</p>
+    if(loading) return <p>Caregando</p>
+    if(data)
+        return (
+            <div>
+                <p>Produto preferido {produto}</p>
+                <button onClick={handleProdutoClick}>notebook</button>
+                <button onClick={handleProdutoClick}>iphone</button>
 
-    return (
-        <div>
-            <p>Produto preferido {produto}</p>
-            <button onClick={handleProdutoClick}>notebook</button>
-            <button onClick={handleProdutoClick}>iphone</button>
-        </div>
-    );
+                {data.map((produto) => <div key={produto.id}><h1>{produto.nome}</h1></div>)}
+            </div>
+        );
+    else {
+        return null
+    }
 };
 
 export default App;
