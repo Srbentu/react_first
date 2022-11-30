4139cdae011b27747adeb9c48a28983a8ac7ab9e
Breno Marcomini <brenomarcomini@hotmail.com>
2022-11-30 20:56:17 -0300

    Imagens

--- appreact/src/Components/Produto.js
+++ appreact/src/Components/Produto.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Produto.module.css'
+import foto from "../img/evangelion_1.jpg"
 
 const Produto = () => {
-    console.log(styles)
     return(
         <div>
             <h1 className={styles.titulo}>Notebook</h1>
             <p className={styles.preco}>Preço 2000</p>
+            <img src={foto} alt=""/>
             <button>Comprar</button>
         </div>
     )
