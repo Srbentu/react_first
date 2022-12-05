b1555acc970a2d71c7907d7439a7d415de9eb4e0
Breno Marcomini <brenomarcomini@hotmail.com>
2022-12-05 20:42:31 -0300

    Rotas

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Produto from "./Components/Produto";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./Home";
+import Sobre from "./Sobre";
+import Header from "./Header"
+import NaoEncontrado from "./NaoEncontrado";
 
 
 
 const App = () => {
-    return <div>
-        <Produto/>
-    </div>
+    return <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="Sobre" element={<Sobre/>}/>
+            <Route path="*" element={<NaoEncontrado/>}/>
+        </Routes>
+    </BrowserRouter>
 
 };
 
